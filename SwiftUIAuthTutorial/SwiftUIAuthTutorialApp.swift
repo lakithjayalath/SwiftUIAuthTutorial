c3da262f7d6ac9c1398da0bb1a0f5e82a38012ec
lakithjayalath <j.lakith@infoplaza.nl>
2023-10-31 17:49:08 +0530

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Lakith Jayalath on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
